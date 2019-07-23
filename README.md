96e7ccbda82c35a4d3a96699fe2fc4918329fa53
Iniquit <iniquitwrites@gmail.com>
2019-07-23 10:45:02 -0400

    Secondary Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # scribe
 A lightweight browser-pased parser for MagicSpells java files. 
+
+https://iniquit.github.io/scribe/
--- script.js
+++ script.js
@@ -13,31 +13,40 @@ let str = document.getElementsByClassName("test")[0].value;
 
 if (str === ""){return;}
 
-let reg = /(getConfig([^("]+))\(([^\)]+)\)/g;
-let reg2 = /getConfig([^("]+)\(([^\)]+)\)/g;
+let final = Array.from(str.matchAll(/getConfig([^("]+)\(([^\)]+)\)/g));
 
-let final = Array.from(str.matchAll(reg2));
-
-//alert(str); 
-//alert(final); // Array: ["<h1>", "h1"]
 let namereg = /class\s*(\S+)/
 let quotereg = /(["'])(?:(?=(\\?))\2.)*?\1/g
 let extendsreg = /package com\.nisovin\.magicspells\.spells(\.[a-z]+)/
+let optionreg = /"(.*)",(.*$)/g
 
 let spellname = ""
 let spellextend = ""
+let options = []
 
-if ((str.match(namereg)) === null){spellname = "?"}
+
+
+if ((str.match(namereg)) === null){
+    spellname = "?"
+}
+
 else {
     spellname = Array.from(str.match(namereg))[1];
 }
 
-if ((str.match(extendsreg)) === null){spellextend = ""}
+
+
+if ((str.match(extendsreg)) === null){
+    spellextend = ""
+}
+
 else {
     spellextend = Array.from(str.match(extendsreg))[1];
 }
 
-console.log(spellextend)
+
+
+//console.log(spellextend)
 
 let temp = ""
 
@@ -58,19 +67,26 @@ final.forEach(e => {
     else if (e[1] === "Vector"){temp ="vector"}
     else {temp = e[1]}
 
+    if ((e[2].match(optionreg)) === null) {
+        options = "No options found"
+    }
 
-if (showVariableTypes){
-    output.push("    " + e[2] + '<span class="grey"> #' + temp + "</span>")
-}
+    else {
+        options = Array.from(e[2].matchAll(optionreg));
+    }
 
-else {
-    output.push("    " + e[2])
-}
+console.log(options)
+
+    if (showVariableTypes){
+        output.push("    " + options[0][1] + ":" + options[0][2] + '<span class="grey"> #' + temp + "</span>")
+    }
+
+    else {
+        output.push("    " + options[0][1] + ":" + options[0][2])
+    }
 
     
 });
-
-
 
 output = output.join('\n');
 
@@ -78,8 +94,8 @@ output = output.join('\n');
 
 
 
-document.getElementsByClassName("demo")[0].innerHTML = spellname + "\n" + output
+document.getElementsByClassName("demo")[0].innerHTML = spellname + ":\n" + output
 
 }
 
-myFunction()
+//myFunction()
